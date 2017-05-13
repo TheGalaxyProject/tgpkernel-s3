564c8231f6bde3545bc050bd726b6e73340a8bfd
djb77 <dwayne.bakewell@gmail.com>
2017-05-13 14:33:47 +1000

    selinux: Allow SELinux to be Permissive or Enforcing
    Signed-off-by: djb77 <dwayne.bakewell@gmail.com>

--- security/selinux/exports.c
+++ security/selinux/exports.c
@@ -17,10 +17,11 @@
 
 bool selinux_is_enabled(void)
 {
-#ifdef CONFIG_ALWAYS_ENFORCE
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 	return true;
 #else
 	return selinux_enabled;
 #endif
 }
 EXPORT_SYMBOL_GPL(selinux_is_enabled);
+
--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -168,7 +168,7 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	length = -EINVAL;
 	if (sscanf(page, "%d", &new_value) != 1)
 		goto out;
-#ifdef CONFIG_ALWAYS_ENFORCE
+#ifdef CONFIG_SECURITY_SELINUX_ALWAYS_ENFORCE
 	// If build is user build and enforce option is set, selinux is always enforcing
 	new_value = 1;
 	length = task_has_security(current, SECURITY__SETENFORCE);
@@ -181,6 +181,17 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	avc_ss_reset(0);
 	selnl_notify_setenforce(new_value);
         selinux_status_update_setenforce(new_value);
+#elif defined(CONFIG_SECURITY_SELINUX_NEVER_ENFORCE)
+ 	// If build is user build and permissive option is set, selinux is always permissive
+ 	new_value = 0;
+ 	length = task_has_security(current, SECURITY__SETENFORCE);
+ 	audit_log(current->audit_context, GFP_KERNEL, AUDIT_MAC_STATUS,
+                         "config_never_enforce - true; enforcing=%d old_enforcing=%d auid=%u ses=%u",
+                         new_value, selinux_enforcing,
+                         audit_get_sessionid(current));
+ 	selinux_enforcing = new_value;
+ 	selnl_notify_setenforce(new_value);
+ 	selinux_status_update_setenforce(new_value);
 #else
 	if (new_value != selinux_enforcing) {
 		length = task_has_security(current, SECURITY__SETENFORCE);
